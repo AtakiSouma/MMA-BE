6b32faeb1b4963c33ef2f421abee301be18c22f1
AtakiSouma <atakisouma394@gmail.com>
2024-04-22 19:45:03 +0700

    result

--- src/routes/course.route.ts
+++ src/routes/course.route.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, Application } from 'express'
+import { copyFile } from 'fs'
 import courseController from '~/controllers/course.controller'
 import MiddleWareController from '~/middlewares/auth'
 import courseServices from '~/services/course.services'
@@ -13,4 +14,6 @@ router.put('/update-statuss', MiddleWareController.isAuthenticated, courseContro
 router.put('/toggle-block', MiddleWareController.isAuthenticated, courseController.ToggleBlockCourse)
 router.get('/get-all-courses', MiddleWareController.isAuthenticated, courseController.getAllCourseInMobile)
 router.get('/get-all/bought/:userId', MiddleWareController.isAuthenticated, courseController.getAllCourseBoughtByUser)
+router.post('/add-question', MiddleWareController.isAuthenticated, courseController.AddANewQuestion)
+router.post("/add-answer" , MiddleWareController.isAuthenticated , courseController.NewAnswer)
 export default router
