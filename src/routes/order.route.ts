c4de42864ee04c8be57e6517098d909ec7181e80
AtakiSouma <atakisouma394@gmail.com>
2024-04-23 13:06:12 +0700

    saves

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -4,5 +4,5 @@ import MiddleWareController from '~/middlewares/auth'
 const router = express.Router()
 router.post('/create', MiddleWareController.isAuthenticated, orderController.createOrder)
 router.post('/get-all', MiddleWareController.isAuthenticated, orderController.getAllOrder)
-
+router.get('/user/:userId', MiddleWareController.isAuthenticated, orderController.getAllOrderByUser)
 export default router
